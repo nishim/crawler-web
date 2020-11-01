f1efe1bce98dfe5952111ca770a93d8197ce5947
Yuji Nishimura <nishimura.yuji@liclab.com>
2020-11-02 00:44:06 +0900

    init

---
+++ docker/postgresql/testdb.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE test
+    WITH
+    OWNER = pg
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'ja_JP.utf8'
+    LC_CTYPE = 'ja_JP.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
