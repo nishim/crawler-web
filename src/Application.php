7c1322434db03b124a34f6581e9076eebcdd9f89
Yuji Nishimura <nishimura.yuji@liclab.com>
2020-11-07 02:22:22 +0900

    idehelper

--- src/Application.php
+++ src/Application.php
@@ -41,6 +41,8 @@ class Application extends BaseApplication
      */
     public function bootstrap(): void
     {
+        $this->addPlugin('IdeHelper');
+
         // Call parent to load bootstrap from files.
         parent::bootstrap();
 
